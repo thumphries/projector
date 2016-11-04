39e4ebde4e15154063589185ea4784f5b7e16c6a
Tim Humphries <tim.humphries@ambiata.com>
2016-11-04 15:30:28 +1100

    HTML types

---
+++ projector-html/src/Projector/Html/Core/Prim.hs
@@ -0,0 +1,70 @@
+-- | Everything in this module must be made available in the target
+-- prior to codegen. i.e. these are the primitives we assume.
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+module Projector.Html.Core.Prim (
+    HtmlType
+  , HtmlDecl
+  , HtmlDecls
+  , HtmlExpr
+  , PrimT (..)
+  , types
+  , tBool
+  , dBool
+  ) where
+
+
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+
+import           P
+
+import           Projector.Core
+
+
+type HtmlType = Type PrimT
+type HtmlDecl = Decl PrimT
+type HtmlDecls = TypeDecls PrimT
+type HtmlExpr = Expr PrimT
+
+data PrimT
+  = TString
+  deriving (Eq, Ord, Read, Show)
+
+instance Ground PrimT where
+  data Value PrimT
+    = VString Text
+    deriving (Eq, Ord, Read, Show)
+
+  typeOf v = case v of
+    VString _ -> TString
+
+  ppGroundType t = case t of
+    TString -> "String"
+
+  ppGroundValue v = case v of
+    VString s ->
+      T.pack (show s)
+
+types :: HtmlDecls
+types =
+  TypeDecls $ M.fromList [
+      (nBool, dBool)
+    ]
+
+
+nBool :: TypeName
+nBool =
+  TypeName "Bool"
+
+tBool :: HtmlType
+tBool =
+  TVar nBool
+
+dBool :: HtmlDecl
+dBool =
+  DVariant [
+      (Constructor "True", [])
+    , (Constructor "False", [])
+    ]
