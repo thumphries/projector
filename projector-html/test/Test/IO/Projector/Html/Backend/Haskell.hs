18b18a424368d6c9c4617b4c377378c1e2c96b65
Tim Humphries <tim.humphries@ambiata.com>
2016-11-21 12:23:44 +1100

    Clean up

--- projector-html/test/Test/IO/Projector/Html/Backend/Haskell.hs
+++ projector-html/test/Test/IO/Projector/Html/Backend/Haskell.hs
@@ -5,6 +5,8 @@
 module Test.IO.Projector.Html.Backend.Haskell where
 
 
+import           Control.Exception (catch, throwIO)
+
 import qualified Data.List as L
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -20,7 +22,10 @@ import           Projector.Html.Core.Prim
 import qualified Projector.Html.Core.Library as Lib
 import           Projector.Html.Backend.Haskell
 
+import           System.Directory  (removeFile)
 import           System.Exit (ExitCode(..))
+import           System.IO (IO, FilePath)
+import           System.IO.Error  (isDoesNotExistError)
 import           System.Process (readProcessWithExitCode)
 
 
@@ -44,9 +49,13 @@ ghcProp :: ModuleName -> Text -> Property
 ghcProp (ModuleName n) modl =
   testIO $ do
     -- TODO convert module names to valid nested paths
-    let path = "./dist/" <> T.unpack n <> ".hs"
+    let base = "./dist/" <> T.unpack n
+        path = base <> ".hs"
     T.writeFile path modl
     (code, _out, err) <- readProcessWithExitCode "cabal" ["exec", "--", "ghc", path] ""
+    removeFile path
+    removeIfExists (base <> ".hi")
+    removeIfExists (base <> ".o")
     case code of
       ExitSuccess ->
         pure (property True)
@@ -56,6 +65,12 @@ ghcProp (ModuleName n) modl =
               , err
               ]
         in pure $ counterexample errm (property False)
+
+removeIfExists :: FilePath -> IO ()
+removeIfExists fileName = removeFile fileName `catch` handleExists
+  where handleExists e
+          | isDoesNotExistError e = return ()
+          | otherwise = throwIO e
 
 
 return []
