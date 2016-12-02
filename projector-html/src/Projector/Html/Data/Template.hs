a2153c09db4eb929f4279fc3245d3afb650ded77
Tim Humphries <tim.humphries@ambiata.com>
2016-12-02 17:33:11 +1100

    comonads

--- projector-html/src/Projector/Html/Data/Template.hs
+++ projector-html/src/Projector/Html/Data/Template.hs
@@ -28,6 +28,8 @@ module Projector.Html.Data.Template (
   ) where
 
 
+import           Control.Comonad  (Comonad(..))
+
 import           Data.List.NonEmpty  (NonEmpty(..))
 
 import           P
@@ -37,18 +39,51 @@ data Template a
   = Template a (Maybe (TTypeSig a)) (THtml a)
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
 
+instance Comonad Template where
+  extract (Template a _ _) =
+    a
+  extend f t@(Template _ g h) =
+    Template (f t) (fmap (extend (const (f t))) g) (extend (const (f t)) h)
+
 data TTypeSig a
   = TTypeSig a (NonEmpty (TId, TType a))
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+instance Comonad TTypeSig where
+  extract (TTypeSig a _) =
+    a
+  extend f ts@(TTypeSig _ tss) =
+    TTypeSig (f ts) (fmap (fmap (extend (const (f ts)))) tss)
 
 data TType a
   = TTVar a TId
   | TTApp a (TType a) (TType a)
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
 
+instance Comonad TType where
+  extract ty =
+    case ty of
+      TTVar a _ ->
+        a
+      TTApp a _ _ ->
+        a
+  extend f ty =
+    case ty of
+      TTVar _ x ->
+        TTVar (f ty) x
+      TTApp _ t1 t2 ->
+        TTApp (f ty) (extend f t1) (extend f t2)
+
 data THtml a
   = THtml a [TNode a]
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+instance Comonad THtml where
+  extract (THtml a _) =
+    a
+  extend f h@(THtml _ nodes) =
+    THtml (f h) (fmap (extend (const (f h))) nodes)
+
 
 data TNode a
   = TElement a TTag [TAttribute a] (THtml a)
@@ -59,10 +94,59 @@ data TNode a
   | TExprNode a (TExpr a)
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
 
+instance Comonad TNode where
+  extract node =
+    case node of
+      TElement a _ _ _ ->
+        a
+      TVoidElement a _ _ ->
+        a
+      TComment a _ ->
+        a
+      TWhiteSpace a ->
+        a
+      TExprNode a _ ->
+        a
+      TPlain a _ ->
+        a
+  extend f node =
+    case node of
+      TElement _ t a h ->
+        TElement
+          (f node)
+          t
+          (fmap (extend (const (f node))) a)
+          (extend (const (f node)) h)
+      TVoidElement _ t a ->
+        TVoidElement (f node) t (fmap (extend (const (f node))) a)
+      TComment _ t ->
+        TComment (f node) t
+      TWhiteSpace _ ->
+        TWhiteSpace (f node)
+      TExprNode _ e ->
+        TExprNode (f node) (extend (const (f node)) e)
+      TPlain _ t ->
+        TPlain (f node) t
+
+
 data TAttribute a
   = TAttribute a TAttrName (TAttrValue a)
   | TEmptyAttribute a TAttrName
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+instance Comonad TAttribute where
+  extract attr =
+    case attr of
+      TAttribute a _ _ ->
+        a
+      TEmptyAttribute a _ ->
+        a
+  extend f attr =
+    case attr of
+      TAttribute _ n v ->
+        TAttribute (f attr) n (extend (const (f attr)) v)
+      TEmptyAttribute _ n ->
+        TEmptyAttribute (f attr) n
 
 data TAttrValue a
   = TQuotedAttrValue a TPlainText
@@ -70,15 +154,53 @@ data TAttrValue a
   | TAttrExpr a (TExpr a)
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
 
+instance Comonad TAttrValue where
+  extract val =
+    case val of
+      TQuotedAttrValue a _ ->
+        a
+      TUnquotedAttrValue a _ ->
+        a
+      TAttrExpr a _ ->
+        a
+  extend f expr = case expr of
+    TQuotedAttrValue _ t -> TQuotedAttrValue (f expr) t
+    TUnquotedAttrValue _ t -> TUnquotedAttrValue (f expr) t
+    TAttrExpr _ e -> TAttrExpr (f expr) (extend (const (f expr)) e)
+
 data TExpr a
   = TEVar a TId
   | TEApp a (TExpr a) (TExpr a)
   | TECase a (TExpr a) (NonEmpty (TAlt a))
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
 
+instance Comonad TExpr where
+  extract expr =
+    case expr of
+      TEVar a _ ->
+        a
+      TEApp a _ _ ->
+        a
+      TECase a _ _ ->
+        a
+  extend f expr =
+    case expr of
+      TEVar _ a ->
+        TEVar (f expr) a
+      TEApp _ e1 e2 ->
+        TEApp (f expr) (extend f e1) (extend f e2)
+      TECase _ e alts ->
+        TECase (f expr) (extend f e) (fmap (extend (const (f expr))) alts)
+
 data TAlt a
   = TAlt a (TPattern a) (TAltBody a)
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+instance Comonad TAlt where
+  extract (TAlt a _ _) =
+    a
+  extend f a@(TAlt _ p b) =
+    TAlt (f a) (extend (const (f a)) p) (extend (const (f a)) b)
 
 -- FIX only Element / VoidElements are valid here
 -- should make this correct by construction
@@ -87,10 +209,38 @@ data TAltBody a
   | TAltHtml a (THtml a)
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
 
+instance Comonad TAltBody where
+  extract body =
+    case body of
+      TAltExpr a _ ->
+        a
+      TAltHtml a _ ->
+        a
+  extend f body =
+    case body of
+      TAltExpr _ e ->
+        TAltExpr (f body) (extend (const (f body)) e)
+      TAltHtml _ h ->
+        TAltHtml (f body) (extend (const (f body)) h)
+
 data TPattern a
   = TPVar a TId
   | TPCon a TConstructor [TPattern a]
   deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+instance Comonad TPattern where
+  extract pat =
+    case pat of
+      TPVar a _ ->
+        a
+      TPCon a _ _ ->
+        a
+  extend f pat =
+    case pat of
+      (TPVar _ a) ->
+        TPVar (f pat) a
+      TPCon _ a b ->
+        TPCon (f pat) a (fmap (extend f) b)
 
 newtype TId = TId { unTId :: Text }
   deriving (Eq, Ord, Show)
