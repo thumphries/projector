354fe2bd623fc4de1a0d37a15c9c4290d82b1ea1
Tim Humphries <tim.humphries@ambiata.com>
2016-12-20 13:38:08 +1100

    Report cycles

--- projector-core/src/Projector/Core/CallGraph.hs
+++ projector-core/src/Projector/Core/CallGraph.hs
@@ -4,13 +4,15 @@
 module Projector.Core.CallGraph (
     CallGraph (..)
   , buildCallGraph
-  , hasCycles
+  , detectCycles
+  , CycleError (..)
   ) where
 
 
 import qualified Data.Graph as G
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import           Data.Set (Set)
 import qualified Data.Set as S
 import qualified Data.Tree as T
 
@@ -19,21 +21,34 @@ import           P
 import           Projector.Core.Syntax
 
 
+data CycleError
+  = CycleError [[Name]]
+  deriving (Eq, Ord, Show)
+
 data CallGraph = CallGraph {
-    callGraph :: G.Graph
-  , lookupByVertex :: G.Vertex -> (Name, Name, [Name])
-  , lookupByName :: Name -> Maybe G.Vertex
-  }
+    unCallGraph :: Map Name (Set Name)
+  } deriving (Eq, Ord, Show)
 
 buildCallGraph :: Map Name (Expr l a) -> CallGraph
-buildCallGraph m =
-  let (g, lv, ln) =
+buildCallGraph =
+  CallGraph . fmap gatherFree
+
+-- | Report an error if the call graph does not form a DAG.
+-- This does not return an error for free variables or reflexive edges.
+detectCycles :: CallGraph -> Either CycleError ()
+detectCycles cg =
+  let (g, lv, _) =
           G.graphFromEdges
         . fmap (\(n, es) -> (n, n, S.toList es))
         . M.toList
-        $ fmap gatherFree m
-  in CallGraph g lv ln
-
-hasCycles :: CallGraph -> Bool
-hasCycles cgr =
-  not (all (null . T.subForest) (G.scc (callGraph cgr)))
+        $ unCallGraph cg
+      sccs = G.scc g
+      -- for each cycle, take the first, simplest path for error reporting.
+      path n = T.rootLabel n : case T.subForest n of [] -> []; (x:_) -> path x
+      labelled = fmap ((\(a, _, _) -> a) . lv)
+  in case filter (not . null . T.subForest) sccs of
+       [] ->
+         pure ()
+       xs ->
+         trace (show sccs) $
+         Left (CycleError (fmap (labelled . path) xs))
