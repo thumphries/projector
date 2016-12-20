b06ec3637225d866257757d9785bae9f00b48c4f
Tim Humphries <tim.humphries@ambiata.com>
2016-12-20 12:36:03 +1100

    Detect graph cycles

--- projector-core/src/Projector/Core/CallGraph.hs
+++ projector-core/src/Projector/Core/CallGraph.hs
@@ -4,21 +4,36 @@
 module Projector.Core.CallGraph (
     CallGraph (..)
   , buildCallGraph
+  , hasCycles
   ) where
 
 
+import qualified Data.Graph as G
 import           Data.Map.Strict (Map)
-import           Data.Set (Set)
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
+import qualified Data.Tree as T
 
 import           P
 
 import           Projector.Core.Syntax
 
 
-newtype CallGraph = CallGraph {
-    unCallGraph :: Map Name (Set Name)
-  } deriving (Eq, Show, Monoid)
+data CallGraph = CallGraph {
+    callGraph :: G.Graph
+  , lookupByVertex :: G.Vertex -> (Name, Name, [Name])
+  , lookupByName :: Name -> Maybe G.Vertex
+  }
 
 buildCallGraph :: Map Name (Expr l a) -> CallGraph
-buildCallGraph =
-  CallGraph . fmap gatherFree
+buildCallGraph m =
+  let (g, lv, ln) =
+          G.graphFromEdges
+        . fmap (\(n, es) -> (n, n, S.toList es))
+        . M.toList
+        $ fmap gatherFree m
+  in CallGraph g lv ln
+
+hasCycles :: CallGraph -> Bool
+hasCycles cgr =
+  not (all (null . T.subForest) (G.scc (callGraph cgr)))
