6d868c5343785da4e8efc4fa06ae0c841b0138e1
Tim Humphries <tim.humphries@ambiata.com>
2016-12-30 11:36:20 +1100

    Factor out backend IO

--- projector-html/test/Test/IO/Projector/Html/Backend/Haskell.hs
+++ projector-html/test/Test/IO/Projector/Html/Backend/Haskell.hs
@@ -7,11 +7,8 @@ module Test.IO.Projector.Html.Backend.Haskell where
 
 import qualified Data.List as L
 import qualified Data.Map.Strict as M
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
 
 import           Disorder.Core
-import           Disorder.Core.IO
 import           Disorder.Jack
 
 import           P
@@ -22,13 +19,9 @@ import qualified Projector.Html.Core.Library as Lib
 import           Projector.Html.Backend.Data
 import           Projector.Html.Backend.Haskell
 
-import           System.Directory (createDirectoryIfMissing)
-import           System.Exit (ExitCode(..))
-import           System.FilePath.Posix ((</>), (<.>), takeDirectory)
-import           System.IO (FilePath, IO)
-import           System.IO.Temp (withTempDirectory)
 import           System.Process (readProcessWithExitCode)
 
+import           Test.IO.Projector.Html.Backend.Property  (processProp, fileProp)
 import           Test.Projector.Core.Arbitrary (freshNames)
 import           Test.Projector.Html.Arbitrary
 
@@ -114,27 +107,6 @@ runProp mname modl cb =
     (\path -> readProcessWithExitCode "cabal" ["exec", "--", "runhaskell", path] "")
     (processProp cb)
 
-processProp :: ([Char] -> Property) -> (ExitCode, [Char], [Char]) -> Property
-processProp f (code, out, err) =
-  case code of
-    ExitSuccess ->
-      f out
-    ExitFailure i ->
-      let errm = L.unlines [
-              "Process exited with failing status: " <> T.unpack (renderIntegral i)
-            , err
-            ]
-      in counterexample errm (property False)
-
-
-fileProp :: FilePath -> Text -> (FilePath -> IO a) -> (a -> Property) -> Property
-fileProp mname modl f g =
-  testIO . withTempDirectory "./dist/" "gen-hs-XXXXXX" $ \tmpDir -> do
-    let path = tmpDir </> mname <.> "hs"
-        dir = takeDirectory path
-    createDirectoryIfMissing True dir
-    T.writeFile path modl
-    fmap g (f path)
 
 return []
 tests = $disorderCheckEnvAll TestRunFewer
