7e1ab013e899f6d8d040116cc654c6a4bcf95e09
Tim Humphries <tim.humphries@ambiata.com>
2017-01-23 17:01:39 +1100

    Use FixT

--- projector-core/src/Projector/Core/Rewrite.hs
+++ projector-core/src/Projector/Core/Rewrite.hs
@@ -1,44 +1,51 @@
 {- | User-supplied rewrite rules -}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Projector.Core.Rewrite (
     RewriteRule (..)
   , rewrite
   , rewriteFix
-  , applyRule
-  , applyRules
   ) where
 
+
+import           Data.Functor.Identity (Identity(..), runIdentity)
 
 import           P
 
 import           Projector.Core.Syntax
 import           Projector.Core.Type
 
+import           Umami.Monad.FixT (FixT (..))
+import qualified Umami.Monad.FixT as U
+
 
 data RewriteRule l a
   = Rewrite (Expr l a -> Maybe (Expr l a))
 
+
+rewrite :: Ground l => [RewriteRule l a] -> Expr l a -> Expr l a
+rewrite rules =
+  runIdentity . U.once . rewriteT rules
+
+rewriteFix :: Ground l => [RewriteRule l a] -> Expr l a -> Expr l a
+rewriteFix rules =
+  runIdentity . U.fixpoint (rewriteT rules)
+
 -- | Apply a single rewrite rule to a single expr, nonrecursively.
-applyRule :: Ground l => Expr l a -> RewriteRule l a -> Expr l a
+applyRule :: Ground l => Expr l a -> RewriteRule l a -> FixT Identity (Expr l a)
 applyRule g (Rewrite f) =
-  -- This is about as stupid as it gets.
-  fromMaybe g (f g)
+  maybe (pure g) U.progress (f g)
 {-# INLINE applyRule #-}
 
 -- | Apply a set of rewrite rules to a single expr, nonrecursively.
-applyRules :: Ground l => Expr l a -> [RewriteRule l a] -> Expr l a
+applyRules :: Ground l => Expr l a -> [RewriteRule l a] -> FixT Identity (Expr l a)
 applyRules =
-  foldl' applyRule
+  foldM applyRule
 {-# INLINE applyRules #-}
 
--- This scales poorly with the number of rules provided. If/when
--- performance of this code is an issue, we should fuse the rules
--- together into a trie of sorts.
-
--- | Perform a bottom-up single pass over an expr with a set of rewrite rules.
-rewrite :: Ground l => [RewriteRule l a] -> Expr l a -> Expr l a
-rewrite rules expr =
+rewriteT :: Ground l => [RewriteRule l a] -> Expr l a -> FixT Identity (Expr l a)
+rewriteT rules expr =
   case expr of
     ELit _ _ ->
       applyRules expr rules
@@ -46,20 +53,20 @@ rewrite rules expr =
       applyRules expr rules
     EForeign _ _ _ ->
       applyRules expr rules
-    ELam a n t e ->
-      applyRules (ELam a n t (rewrite rules e)) rules
-    EApp a e1 e2 ->
-      applyRules (EApp a (rewrite rules e1) (rewrite rules e2)) rules
-    ECon a c t es ->
-      applyRules (ECon a c t (fmap (rewrite rules) es)) rules
-    ECase a e pes ->
-      applyRules (ECase a (rewrite rules e) (fmap (fmap (rewrite rules)) pes)) rules
-    EList a t es ->
-      applyRules (EList a t (fmap (rewrite rules) es)) rules
-
--- | Call 'rewrite' until the expression stops changing.
--- This assumes that your rule system is terminating.
-rewriteFix :: Ground l => Eq a => [RewriteRule l a] -> Expr l a -> Expr l a
-rewriteFix rules expr =
-  let expr' = rewrite rules expr
-  in if expr' /= expr then rewriteFix rules expr' else expr
+    ELam a n t e -> do
+      e' <- rewriteT rules e
+      applyRules (ELam a n t e') rules
+    EApp a e1 e2 -> do
+      e1' <- rewriteT rules e1
+      e2' <- rewriteT rules e2
+      applyRules (EApp a e1' e2') rules
+    ECon a c t es -> do
+      es' <- traverse (rewriteT rules) es
+      applyRules (ECon a c t es') rules
+    ECase a e pes -> do
+      e' <- rewriteT rules e
+      pes' <- for pes (\(p, ex) -> fmap (p,) (rewriteT rules ex))
+      applyRules (ECase a e' pes') rules
+    EList a t es -> do
+      es' <- traverse (rewriteT rules) es
+      applyRules (EList a t es') rules
