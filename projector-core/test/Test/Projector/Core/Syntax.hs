d9093b9ab2894e0a8ca4bc6db7d303a93da1faba
Tim Humphries <tim.humphries@ambiata.com>
2017-01-27 12:38:27 +1100

    Add failing tests

---
+++ projector-core/test/Test/Projector/Core/Syntax.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+module Test.Projector.Core.Syntax where
+
+
+import           Control.Monad.Trans.State
+
+import           Disorder.Core
+import           Disorder.Jack
+
+import           P
+
+import           Projector.Core.Syntax
+
+import           Test.Projector.Core.Arbitrary
+
+
+-- (foldlExpr extractAnnotation) should correspond to foldl
+prop_foldlExpr =
+  gamble genExprIntAnn $ \expr ->
+    foldlExpr (\acc e -> acc <> [extractAnnotation e]) [] expr
+    ===
+    foldl' (\acc a -> acc <> [a]) [] expr
+
+-- (foldlExprM extractAnnotation) should correspond to foldlM
+prop_foldlExprM =
+  gamble genExprIntAnn $ \expr ->
+    runState (foldlExprM (\acc e -> fun acc (extractAnnotation e)) 0 expr) []
+    ===
+    runState (foldlM fun 0 expr) []
+    where
+      fun acc a = do
+        modify' (<> [a])
+        pure (acc + 1 :: Integer)
+
+-- (foldrExpr extractAnnotation) should correspond to foldr
+prop_foldrExpr =
+  gamble genExprIntAnn $ \expr ->
+    foldrExpr (\e acc -> extractAnnotation e : acc) [] expr
+    ===
+    foldr (\a acc -> a:acc) [] expr
+
+-- (foldrExprM extractAnnotation) should correspond to foldlM
+prop_foldrExprM =
+  gamble genExprIntAnn $ \expr ->
+    runState (foldrExprM (\e acc -> fun (extractAnnotation e) acc) 0 expr) []
+    ===
+    runState (foldrM fun 0 expr) []
+    where
+      fun a acc = do
+        modify' (a:)
+        pure (acc + 1 :: Integer)
+
+genExprIntAnn :: Jack (Expr TestLitT Int)
+genExprIntAnn =
+  genTestExpr >>= traverse (const arbitrary)
+
+return []
+tests = $disorderCheckEnvAll TestRunNormal
--- projector-core/test/test.hs
+++ projector-core/test/test.hs
@@ -2,10 +2,12 @@ import           Disorder.Core.Main
 
 import qualified Test.Projector.Core.Check as Check
 import qualified Test.Projector.Core.Simplify as Simplify
+import qualified Test.Projector.Core.Syntax as Syntax
 
 main :: IO ()
 main =
   disorderMain [
       Check.tests
     , Simplify.tests
+    , Syntax.tests
     ]
