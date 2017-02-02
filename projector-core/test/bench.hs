c83449bf91f9d11f5182c8ac151a8986e1e838e6
Tim Humphries <tim.humphries@ambiata.com>
2017-02-02 15:37:48 +1100

    Slash and burn

--- projector-core/test/bench.hs
+++ projector-core/test/bench.hs
@@ -16,7 +16,7 @@ import           Projector.Core
 import           System.IO (IO)
 
 import           Test.Projector.Core.Arbitrary
-import           Test.Projector.Core.Simplify (mul, nth)
+-- import           Test.Projector.Core.Simplify (mul, nth)
 
 
 -- big lambda billy
@@ -75,55 +75,29 @@ main = do
           , csvFile = Just "dist/build/projector-bench.csv"
           }
 
-      norm f = C.whnf nf . f
       tc f c = C.whnf (typeCheck c) . f
       mul2 = mul 2
 
   defaultMainWith cfg [
-      bgroup "normalise-intlist" [
-          bench "normalise-intlist-100" $ norm buildIntList 100
-        , bench "normalise-intlist-200" $ norm buildIntList 200
-        , bench "normalise-intlist-1000" $ norm buildIntList 1000
-        ]
-    , bgroup "check-intlist" [
+      bgroup "check-intlist" [
           bench "check-intlist-100" $ tc buildIntList tintlistctx 100
         , bench "check-intlist-200" $ tc buildIntList tintlistctx 200
         , bench "check-intlist-1000" $ tc buildIntList tintlistctx 1000
-        ]
-    , bgroup "normalise-billy" [
-          bench "normalise-billy-100" $ norm buildExpr 100
-        , bench "normalise-billy-200" $ norm buildExpr 200
-        , bench "normalise-billy-1000" $ norm buildExpr 1000
         ]
     , bgroup "check-billy" [
           bench "check-billy-100" $ tc buildExpr mempty 100
         , bench "check-billy-200" $ tc buildExpr mempty 200
         , bench "check-billy-1000" $ tc buildExpr mempty 1000
         ]
-    , bgroup "normalise-church" [
-          bench "normalise-church-100" $ norm nth 100
-        , bench "normalise-church-200" $ norm nth 200
-        , bench "normalise-church-1000" $ norm nth 1000
-        ]
     , bgroup "check-church" [
           bench "check-church-100" $ tc nth mempty 100
         , bench "check-church-200" $ tc nth mempty 200
         , bench "check-church-1000" $ tc nth mempty 1000
         ]
-    , bgroup "normalise-church-mul2" [
-          bench "normalise-church-mul2-100" $ norm mul2 100
-        , bench "normalise-church-mul2-200" $ norm mul2 200
-        , bench "normalise-church-mul2-1000" $ norm mul2 1000
-        ]
     , bgroup "check-church-mul2" [
           bench "check-church-mul2-100" $ tc mul2 mempty 100
         , bench "check-church-mul2-200" $ tc mul2 mempty 200
         , bench "check-church-mul2-1000" $ tc mul2 mempty 1000
-        ]
-    , bgroup "normalise-casey" [
-          bench "normalise-casey-100" $ norm buildCase 100
-        , bench "normalise-casey-200" $ norm buildCase 200
-        , bench "normalise-casey-1000" $ norm buildCase 1000
         ]
     , bgroup "check-casey" [
           bench "check-casey-100" $ tc buildCase caseyCtx 100
@@ -131,3 +105,45 @@ main = do
         , bench "check-casey-1000" $ tc buildCase caseyCtx 1000
         ]
     ]
+
+--
+
+unit :: Type TestLitT
+unit =
+  TLit TBool
+
+church :: Type TestLitT
+church =
+  TArrow (TArrow unit unit) (TArrow unit unit)
+
+zero :: Expr TestLitT ()
+zero =
+  lam_ "f" (Just (TArrow unit unit))
+    (lam_ "x" (Just unit) (var_ "x"))
+
+succ :: Expr TestLitT ()
+succ =
+  lam_ "n" (Just church)
+    (lam_ "f" (Just (TArrow unit unit))
+      (lam_ "x" (Just unit)
+        (app
+          (var_ "f")
+          (app (app (var_ "n") (var_ "f")) (var_ "x")))))
+
+mult :: Expr TestLitT ()
+mult =
+  lam_ "m" (Just church)
+    (lam_ "n" (Just church)
+      (lam_ "f" (Just (TArrow unit unit))
+        (app
+          (var_ "m")
+          (app (var_ "n") (var_ "f")))))
+
+nth :: Int -> Expr TestLitT ()
+nth 0 = zero
+nth n = app succ (nth (n - 1))
+
+mul :: Int -> Int -> Expr TestLitT ()
+mul m n =
+  app (app mult (nth m)) (nth n)
+
--- projector-core/test/test.hs
+++ projector-core/test/test.hs
@@ -1,13 +1,11 @@
 import           Disorder.Core.Main
 
 import qualified Test.Projector.Core.Check as Check
-import qualified Test.Projector.Core.Simplify as Simplify
 import qualified Test.Projector.Core.Syntax as Syntax
 
 main :: IO ()
 main =
   disorderMain [
       Check.tests
-    , Simplify.tests
     , Syntax.tests
     ]
