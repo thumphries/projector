43734d47d0c4c8b2617023bacc627652127551ee
Tim Humphries <tim.humphries@ambiata.com>
2017-02-22 13:52:22 +1100

    Add shadowing warnings

--- projector-core/src/Projector/Core.hs
+++ projector-core/src/Projector/Core.hs
@@ -10,3 +10,4 @@ import Projector.Core.Pretty as X
 import Projector.Core.Rewrite as X
 import Projector.Core.Syntax as X
 import Projector.Core.Type as X
+import Projector.Core.Warn as X
---
+++ projector-core/src/Projector/Core/Warn.hs
@@ -0,0 +1,57 @@
+{-| Warnings for things like exhaustivity and shadowing -}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Projector.Core.Warn (
+    Warning (..)
+  , warnShadowing
+  ) where
+
+
+import           Control.Monad.Trans.State (runState, modify')
+
+import           Data.Set (Set)
+import qualified Data.Set as S
+
+import           P
+
+import           Projector.Core.Syntax
+
+
+data Warning l a
+  = ShadowedName a Name
+  deriving (Eq, Ord, Show)
+
+warnShadowing :: Set Name -> Expr l a -> Either [Warning l a] ()
+warnShadowing bound expr =
+  if null it then pure () else Left it
+  where
+    it = (reverse . snd) (runState (go bound expr) mempty)
+    warn w = modify' (w:)
+    go bound' expr' =
+      case expr' of
+        ELit _ _ ->
+          pure ()
+        EVar _ _ ->
+          pure ()
+        EForeign _ _ _ ->
+          pure ()
+        ELam a x _ e -> do
+          when (S.member x bound') (warn (ShadowedName a x))
+          go (S.insert x bound') e
+        EApp _ f g -> do
+          go bound' f
+          go bound' g
+        ECon _ _ _ es -> do
+          traverse_ (go bound') es
+        ECase a e pes -> do
+          go bound' e
+          for_ pes $ \(pat, alt) -> do
+            let bnds = patternBinds pat
+                shad = S.intersection bnds bound'
+            when (shad /= mempty) (for_ (toList shad) (warn . ShadowedName a))
+            go (bnds <> bound') alt
+        EList _ es -> do
+          traverse_ (go bound') es
+        EMap _ f g -> do
+          go bound' f
+          go bound' g
