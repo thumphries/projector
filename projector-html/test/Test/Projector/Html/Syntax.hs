102acd341cd1e6411bf00a6bbf188e08f7bfa186
Tim Humphries <tim.humphries@ambiata.com>
2017-03-29 11:50:46 +1100

    Update regression tests

--- projector-html/test/Test/Projector/Html/Syntax.hs
+++ projector-html/test/Test/Projector/Html/Syntax.hs
@@ -6,19 +6,24 @@
 module Test.Projector.Html.Syntax where
 
 
-import           Data.String.QQ
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 import           Disorder.Core hiding (tripping)
+import           Disorder.Core.IO (testIO)
 import           Disorder.Jack
 
 import           P
 
-import           Projector.Html.Data.Template
+import           Projector.Html
 import           Projector.Html.Pretty
 import           Projector.Html.Syntax
 
+import           System.IO (IO, FilePath)
+
 import           Test.Projector.Html.Arbitrary
+
+import           Text.Show.Pretty (ppShow)
 
 
 prop_parse_roundtrip =
@@ -26,114 +31,59 @@ prop_parse_roundtrip =
     gamble (pure (uglyPrintTemplate t)) $ \_ ->
       tripping
         uglyPrintTemplate
-        (fmap (fmap (const ())) . (templateFromText this))
+        parse
         t
 
--- regression for #193
 prop_parse_unit_prj =
-  once $
-    fmap (fmap (const ())) (templateFromText this "{ foo.bar baz.quux wem.quib pil mun }")
-    ===
-    (Right $
-      Template
-        ()
-        Nothing $
-        TENode () (THtml
-          ()
-          [ TExprNode
-              ()
-              (TEApp
-                 ()
-                 (TEApp
-                    ()
-                    (TEApp
-                       ()
-                       (TEApp
-                          ()
-                          (TEPrj () (TEVar () (TId "foo")) (TId "bar"))
-                          (TEPrj () (TEVar () (TId "baz")) (TId "quux")))
-                       (TEPrj () (TEVar () (TId "wem")) (TId "quib")))
-                    (TEVar () (TId "pil" )))
-                 (TEVar () (TId "mun" )))
-          ]))
+  regressionFile "record_prec"
 
-prop_parse_unit_indent =
-  once $
-    isRight $ fmap (fmap (const ())) (templateFromText this regression_indent)
-
-regression_indent :: Text
-regression_indent =
-  T.pack [s|case x of
-  y ->
-    <a b="{ x
-      }">
-    </a>
-|]
+prop_parse_unit_indent_tag =
+  regressionFile "indent_tag"
 
 prop_parse_unit_html =
-  once $
-    isRight $ fmap (fmap (const ())) (templateFromText this regression_html)
+  regressionFile "html"
 
-regression_html :: Text
-regression_html =
-  T.pack [s|{ foo (<b>bar</b>) <span>baz</span> <h1>quux</hq> }|]
-
-prop_parse_unit_hell =
-  once $
-    isRight $ fmap (fmap (const ())) (templateFromText this regression_hell)
-
-regression_hell :: Text
-regression_hell =
-  T.pack [s|<a>
-  {<b/>}
-</a>
-<c></c>
-|]
+prop_parse_unit_indent_hell =
+  regressionFile "indent_hell"
 
 prop_parse_unit_case =
-  once $
-    isRight $ templateFromText this regression_case
-
-regression_case :: Text
-regression_case =
-  T.pack [s|case foo of
-  Abc ->
-    "def"
-  Ghi ->
-    "jkl"
-  Mno ->
-    "pqr"
-|]
+  regressionFile "case"
 
 prop_parse_unit_brace =
-  once $ isRight $ templateFromText this regression_brace
-
-regression_brace :: Text
-regression_brace =
-  T.pack [s|{ foo bar }
-{ baz quux }
-|]
-
+  regressionFile "brace"
 
 prop_parse_unit_case_html =
-  once $ isRight $ templateFromText this regression_case_html
+  regressionFile "case_html"
 
-regression_case_html :: Text
-regression_case_html =
-  T.pack [s|{
-  case foo of
-    bar ->
-      <blink>party</blink>
-    baz ->
-      <marquee>cube</marquee>
-      <i>abcdefg</i>
-    Bap ->
-      <b>hell</b>
-      <i>foo</i>
-}|]
+prop_parse_unit_conid =
+  regressionFile "conid"
+
+-- -----------------------------------------------------------------------------
 
 this :: [Char]
 this = "Test.Projector.Html.Syntax"
+
+parse :: Text -> Either SyntaxError (Template ())
+parse t =
+  fmap (const ()) <$> templateFromText this t
+
+parse' :: FilePath -> Text -> Either SyntaxError (Template ())
+parse' fp t =
+  fmap (const ()) <$> templateFromText fp t
+
+regressionFile :: FilePath -> Property
+regressionFile fp =
+  once . testIO $ do
+    fin <- T.readFile ("test/syntax/" <> fp <> ".in")
+    out <- T.readFile ("test/syntax/" <> fp <> ".out")
+    pure $
+      fmap (T.pack . ppShow) (parse' fp fin) === pure out
+
+mkRegression :: FilePath -> Text -> IO ()
+mkRegression fp t = do
+  T.writeFile ("test/syntax/" <> fp <> ".in") t
+  for_ (parse' fp t) $
+    T.writeFile ("test/syntax/" <> fp <> ".out") . T.pack . ppShow
 
 return []
 tests = $disorderCheckEnvAll TestRunNormal
